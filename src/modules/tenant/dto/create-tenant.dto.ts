98eafd86989b95761d9ca39fec91a5d160f9ba40
Joel Estumano <joelestumano@gmail.com>
2025-12-26 16:09:51 -0300

    dtos tenant.

--- src/modules/tenant/dto/create-tenant.dto.ts
+++ src/modules/tenant/dto/create-tenant.dto.ts
@@ -1,1 +1,21 @@
-export class CreateTenantDto {}
+import { IsStringValid } from '@common/decorators/is-string.decorator';
+import { ApiProperty } from '@nestjs/swagger';
+import { MaxLength } from 'class-validator';
+
+export class CreateTenantDto {
+  @ApiProperty({
+    description: 'Tenant name must be a non-empty string',
+    example: 'Company LTDA',
+  })
+  @IsStringValid()
+  @MaxLength(100)
+  name: string;
+
+  @ApiProperty({
+    description: 'Tenant domain must be a non-empty string',
+    example: 'company.com',
+  })
+  @IsStringValid()
+  @MaxLength(150)
+  domain: string;
+}
--- src/modules/tenant/tenants.service.ts
+++ src/modules/tenant/tenants.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { TenantEntity } from './entities/tenant.entity';
 
 @Injectable()
-export class TenantsService {}
+export class TenantsService {
+  constructor(
+    @InjectRepository(TenantEntity)
+    private readonly tenantsRepository: Repository<TenantEntity>,
+  ) {}
+}
