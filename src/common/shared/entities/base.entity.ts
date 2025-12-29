a0ce67662f4a5d33c6e40c64655cccaa5f50aab1
Joel Estumano <joelestumano@gmail.com>
2025-12-29 15:08:46 -0300

    Update user profile

--- src/common/shared/entities/base.entity.ts
+++ src/common/shared/entities/base.entity.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export class BaseEntity {
-  @Column({ nullable: false, default: true, select: false })
+  @Column({ nullable: false, default: true })
   active: boolean;
 
   @CreateDateColumn({ type: 'timestamp' })
